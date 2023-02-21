c65ec6e52e565435d104fd90a3b9105af082c4c5
sadhanasidharthan <113581766+sadhanasidharthan@users.noreply.github.com>
2023-02-21 11:59:37 +0530

    Add files via upload

---
+++ Api.java
@@ -0,0 +1,15 @@
+
+package com.example.demo;
+
+public class Api {
+	private String message;
+	public Api(String message)
+	{
+		this.message=message;
+	}
+	public String GetMessage()
+	{
+		return message;
+	}
+
+}
